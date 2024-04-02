a847e6325be1c5f26a5c59980b476480d05e63e3
GuDMuller <61275842+GuDMuller@users.noreply.github.com>
2024-04-02 16:22:33 -0300

    Commit Final
    jogo com todos os botões/funcionalidades implementadas

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //selecionando elementos
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -11,33 +14,80 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //condição inicial
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+};
+
+init();
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // função de rodar o dado
 
 btnRoll.addEventListener('click', function () {
-  //1. Gerar uma rolagem de dado aleatória
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  //2. Mostrar o dado
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  //3. checar se o dado caiu 1
+  if (playing) {
+    //1. Gerar uma rolagem de dado aleatória
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //2. Mostrar o dado
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    //3. checar se o dado caiu 1
 
-  if (dice !== 1) {
-    // adicionar o dado a pontuação atual
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // passar o turno para o proximo jogador
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    if (dice !== 1) {
+      // adicionar o dado a pontuação atual
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // passar o turno para o proximo jogador
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. Adicionar a pontuação atual para o player ativo
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //2. Checar se a pontuação do player é >= 100
+    if (scores[activePlayer] >= 20) {
+      //encerrar o jogo
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--active');
+      // Trocar de player
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
